60afe4b29e08be9e98f340bbf8f86419660146b3
Ramenaru <ramenarudev@gmail.com>
2023-11-15 13:48:33 +0700

    [App] fetch to forecast wheater api

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
-func main () {
-	fmt.Println("ready")
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+		Region  string `json:"region"`
+	} `json:"location"`
+
+	Current struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+		Region  string `json:"region"`
+	} `json:"current"`
+}
+
+func main() {
+	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=9fffb20495d14590a7761327231511&q=bandung")
+
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic("Weather API not Available !")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 }
